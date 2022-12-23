adb11f7442dfd5a473c598afee182a35954cb2f7
Pane2k <82178883+Pane2k@users.noreply.github.com>
2022-12-23 19:25:51 +0300

    statup

---
+++ src/main/java/by/nevyhlas/skachki/HelloServlet.java
@@ -0,0 +1,43 @@
+package by.nevyhlas.skachki;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Login";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+
+
+
+        out.println("<div style=\"position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);\">");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<form action=\"login\" method=\"post\">");
+        out.println("<label for=\"username\">Username:</label>");
+        out.println("<input type=\"text\" id=\"username\" name=\"username\"><br><br>");
+        out.println("<label for=\"password\">Password:</label>");
+        out.println("<input type=\"password\" id=\"password\" name=\"password\"><br><br>");
+        out.println("<input type=\"submit\" value=\"Submit\">");
+        out.println("</form>");
+        out.println("</div>");
+
+
+
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
